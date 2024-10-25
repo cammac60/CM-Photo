a6935e23a4891971358b41ab73b218e6ae5723b8
unknown <cam.mac.cm@gmail.com>
2024-10-25 13:19:13 -0600

    Add font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const DINMittelschrift = localFont({
+  src: "./fonts/DINMittelschriftStd.otf",
+  variable: "--font-DIN",
+  weight: "100",
 });
 
 export const metadata = {
@@ -19,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${DINMittelschrift.variable}`}>
         {children}
       </body>
     </html>
