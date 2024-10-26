6a4b6b750952e2a1c5491fc5397c52c48223b054
unknown <cam.mac.cm@gmail.com>
2024-10-25 20:12:25 -0600

    Add basic header and styling

---
+++ src/app/Header/page.js
@@ -0,0 +1,16 @@
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div id="header-wrapper">
+            <div id="header-left">
+                <h1>Cameron MacRae</h1>
+            </div>
+            <div id="header-right">
+
+            </div>
+        </div>
+    )
+};
+
+export default Header;
