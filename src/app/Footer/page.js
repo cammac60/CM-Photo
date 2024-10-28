66325928f1b7385c1b4c8f4628d6f5dff836f9ef
unknown <cam.mac.cm@gmail.com>
2024-10-28 15:20:05 -0600

    Add paragraph

--- src/app/Footer/page.js
+++ src/app/Footer/page.js
@@ -2,8 +2,15 @@
 import './Footer.scss';
 
 const Footer = () => {
+    const email = 'cam.mac.cm@gmail.com';
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div></div>
+        <div id="footer-wrapper">
+            <p>Copyright Notice</p>
+            <p>© {currentYear} Cameron MacRae Photography & Design. All rights reserved.</p>
+            <p>All images and content on this website are the property of John Doe and are protected by copyright laws. Unauthorized use, reproduction, or distribution of any images or content without prior written permission is strictly prohibited. For licensing inquiries or to obtain permission for use, please contact {email}.</p>
+        </div>
     )
 };
 
