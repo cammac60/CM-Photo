c750504507251d8bc1dcfcadec39c720360afb3d
unknown <cam.mac.cm@gmail.com>
2024-10-28 13:22:02 -0600

    Add links

--- src/app/Header/page.js
+++ src/app/Header/page.js
@@ -1,6 +1,12 @@
+'use client'
 import './Header.scss';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const Header = () => {
+
+    const router = useRouter();
+
     return (
         <div id="header-wrapper">
             <div id="header-left">
@@ -8,7 +14,11 @@ const Header = () => {
                 <h2 id="header-left_sub-title">Photography & Design</h2>
             </div>
             <div id="header-right" class="menu">
-                
+                <Link href="/" id="_home">Home</Link>
+                <Link href="/Gallery" id="_gallery">Gallery</Link>
+                <Link href="/About" id="_about">About</Link>
+                {/* TO DO: Add link to storefront */}
+                <Link href="/shop" id="_shop">Shop</Link>
             </div>
             <div id="header-right">
 
