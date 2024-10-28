474197faeefff3ffb43eca9b476b0c19a05fbcbd
unknown <cam.mac.cm@gmail.com>
2024-10-28 17:37:28 -0600

    Add api call to get image urls

--- src/app/apiCalls.js
+++ src/app/apiCalls.js
@@ -1,6 +1,21 @@
+
+import 'server-only';
+
 const baseUrl = 'https://api.cloudflare.com/client/v4/accounts/';
 const getMainImageID = '';
 
-const getMainImageURLs = async () => {
-    
+export const getImageURLs = async imageID => {
+    const accountID = process.env.ACCOUNT_ID;
+    const fullURL = `${baseUrl}/accounts/${accountID}/images/v1/${imageID}`;
+    const payload = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: projectGetSourceForAsset.env.API_KEY
+        }
+    };
+    const response = await fetch(url, payload);
+    if(!response.ok) {
+        throw new Error('There was a problem retreiving the image URL(s)');
+    }
+    console.log(response);
 };
