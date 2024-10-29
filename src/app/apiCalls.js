0bbb7e1f1146ae195de0c32e5e1d0bde06717984
unknown <cam.mac.cm@gmail.com>
2024-10-28 21:59:03 -0600

    Add image functions and html

--- src/app/apiCalls.js
+++ src/app/apiCalls.js
@@ -2,20 +2,32 @@
 import 'server-only';
 
 const baseUrl = 'https://api.cloudflare.com/client/v4/accounts/';
-const getMainImageID = '';
 
-export const getImageURLs = async imageID => {
+export const getImageVariantUrls = async imageID => {
     const accountID = process.env.ACCOUNT_ID;
-    const fullURL = `${baseUrl}/accounts/${accountID}/images/v1/${imageID}`;
+    const fullURL = `${baseUrl}${accountID}/images/v1/${imageID}`;
     const payload = {
         headers: {
             'Content-Type': 'application/json',
-            Authorization: projectGetSourceForAsset.env.API_KEY
+            Authorization: process.env.API_KEY
         }
     };
-    const response = await fetch(url, payload);
-    if(!response.ok) {
-        throw new Error('There was a problem retreiving the image URL(s)');
-    }
-    console.log(response);
+    const response = await fetch(fullURL, payload);
+    // if(response.status !== 200) {
+    //     console.log(response)
+    //     throw new Error('There was a problem retreiving the image URL(s)');
+    // }
+    const resJSON = await response.json();
+    return resJSON.result.variants;
+};
+
+export const getImage = async (url, type) => {
+    const result = await getImageVariantUrls(url);
+    const formattedUrl = result.filter(variant => variant.endsWith('landscapeWeb'))[0];
+    return formattedUrl;
+};
+
+export const getBannerImage = async () => {
+    const bannerImgUrl = process.env.BANNER_IMG_ID;
+    return getImage(bannerImgUrl, 'landscapeWeb');
 };
