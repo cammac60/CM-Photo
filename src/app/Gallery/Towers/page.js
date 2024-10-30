011a71c54123838f1be20297eb0db3a25a4f56bf
unknown <cam.mac.cm@gmail.com>
2024-10-30 16:19:29 -0600

    Add basic component

--- src/app/Gallery/Towers/page.js
+++ src/app/Gallery/Towers/page.js
@@ -1,1 +1,11 @@
 import './Towers.scss';
+
+const Towers = () => {
+    return (
+        <div id="towers-wrapper" className="gallery-images-wrapper">
+
+        </div>
+    );
+};
+
+export default Towers;
