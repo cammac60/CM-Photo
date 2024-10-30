999d824a703b9bf909febca9fea82cbc7b634990
unknown <cam.mac.cm@gmail.com>
2024-10-30 14:27:42 -0600

    Add image tiles and styling

--- src/app/Gallery/page.js
+++ src/app/Gallery/page.js
@@ -7,7 +7,18 @@ const Gallery = async () => {
     return (
       <div id="gallery">
         <div id="gallery_banner">
+            <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[0].url})`}} height={2048} width={1638}>
 
+            </div>
+            <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[1].url})`}} height={2048} width={1638}>
+
+            </div>
+            <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[2].url})`}} height={2048} width={1638}>
+
+            </div>
+            <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[3].url})`}} height={2048} width={1638}>
+                
+            </div>
         </div>
       </div>
     );
