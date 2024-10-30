d2a81da8aeed2607a7653426cbbb92df77906387
unknown <cam.mac.cm@gmail.com>
2024-10-30 09:53:00 -0600

    Add method for banner images and filter data

--- src/app/Gallery/page.js
+++ src/app/Gallery/page.js
@@ -1,11 +1,13 @@
 import './Gallery.scss';
+import { getGalleryBannerImages } from '../apiCalls.js';
 
 const Gallery = async () => {
-
+    const bannerImages = await getGalleryBannerImages();
+    console.log(bannerImages);
     return (
       <div id="gallery">
         <div id="gallery_banner">
-            
+
         </div>
       </div>
     );
