e92f2e2a21f8f308971036df9e5695a661cf936b
unknown <cam.mac.cm@gmail.com>
2024-10-30 14:36:37 -0600

    Add tile text and additional styling

--- src/app/Gallery/page.js
+++ src/app/Gallery/page.js
@@ -8,16 +8,16 @@ const Gallery = async () => {
       <div id="gallery">
         <div id="gallery_banner">
             <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[0].url})`}} height={2048} width={1638}>
-
+                <p className='gallery_banner-tile_text'>{bannerImages[0].categoryName}</p>
             </div>
             <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[1].url})`}} height={2048} width={1638}>
-
+                <p className='gallery_banner-tile_text'>{bannerImages[1].categoryName}</p>
             </div>
             <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[2].url})`}} height={2048} width={1638}>
-
+                <p className='gallery_banner-tile_text'>{bannerImages[2].categoryName}</p>
             </div>
             <div className="gallery_banner-tile" style={{backgroundImage: `url(${bannerImages[3].url})`}} height={2048} width={1638}>
-                
+                <p className='gallery_banner-tile_text'>{bannerImages[3].categoryName}</p>
             </div>
         </div>
       </div>
