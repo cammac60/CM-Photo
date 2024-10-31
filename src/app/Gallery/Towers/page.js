10b67ae3e0382137b012eb45f993697b21b6e8ca
unknown <cam.mac.cm@gmail.com>
2024-10-31 15:49:43 -0600

    Complete image component and styling for towers gallery

--- src/app/Gallery/Towers/page.js
+++ src/app/Gallery/Towers/page.js
@@ -1,12 +1,14 @@
 import './Towers.scss';
 import { getPhotosByGalleryType } from '../../apiCalls.js';
+import ImageTile from '../../Image/page.js';
 
 const Towers = async () => {
     const data = await getPhotosByGalleryType('Towers');
-    console.log(data);
     return (
         <div id="towers-wrapper" className="gallery-images-wrapper">
-
+            {data.map((item, index) => (
+                <ImageTile key={index} props={item} />
+            ))}
         </div>
     );
 };
--- src/app/Image/page.js
+++ src/app/Image/page.js
@@ -1,30 +1,35 @@
 import Image from 'next/image';
+import './Image.scss';
 
 const ImageTile = async props => {
-    const { url, caption, variantSize } = props;
-    let baseHeight, baseWidth;
+    const { url, caption, variantSize, order } = props.props;
+    let baseHeight, baseWidth, alignment;
     switch (variantSize) {
         case 'portraitWeb':
+            alignment = order % 2 === 0 ? 'right' : 'left';
             baseWidth = 1638;
             baseHeight = 2048;
             break;
         case 'portraitWebTall':
+            alignment = order % 2 === 0 ? 'right' : 'left';
             baseWidth = 1365;
             baseHeight = 2048;
             break;
         case 'landscapeWeb':
+            alignment = 'lanscape';
             baseWidth = 2048;
             baseHeight = 1280;
             break;
         case 'landscapeWebWide':
+            alignment = 'lanscape';
             baseWidth = 2048;
             baseHeight = 512;
             break;
     };
     return (
-        <div className={`gallery-img_${variantSize}`}>
-            <Image src={url} className={variantSize} alt="Image from API" quality={100} overrideSrc='/Img.jpg' height={baseHeight} width={baseWidth} />
-            <p className="gallery-img_caption">{caption}</p>
+        <div className={`gallery-img_${variantSize} gallery-img`}>
+            <Image src={url} className={`${variantSize} img-alignment_${alignment}`} alt="Image from API" quality={100} overrideSrc='/Img.jpg' height={baseHeight} width={baseWidth} />
+            <p className={`gallery-img_caption img-alignment_${alignment}`}>{caption}</p>
         </div>
     );
 };
