0fdf52e13f9a2cd022efdad690c562bed92b0b4b
unknown <cam.mac.cm@gmail.com>
2024-10-31 14:22:45 -0600

    Update images component to be dynamic

--- src/app/Image/page.js
+++ src/app/Image/page.js
@@ -1,12 +1,32 @@
+import Image from 'next/image';
 
-
-const Image = async props => {
-    const { id, url, caption, variantSize } = props;
+const ImageTile = async props => {
+    const { url, caption, variantSize } = props;
+    let baseHeight, baseWidth;
+    switch (variantSize) {
+        case 'portraitWeb':
+            baseWidth = 1638;
+            baseHeight = 2048;
+            break;
+        case 'portraitWebTall':
+            baseWidth = 1365;
+            baseHeight = 2048;
+            break;
+        case 'landscapeWeb':
+            baseWidth = 2048;
+            baseHeight = 1280;
+            break;
+        case 'landscapeWebWide':
+            baseWidth = 2048;
+            baseHeight = 512;
+            break;
+    };
     return (
-        <div id="towers-wrapper" className="gallery-images-wrapper">
-
+        <div className={`gallery-img_${variantSize}`}>
+            <Image src={url} className={variantSize} alt="Image from API" quality={100} overrideSrc='/Img.jpg' height={baseHeight} width={baseWidth} />
+            <p className="gallery-img_caption">{caption}</p>
         </div>
     );
 };
 
-export default Image;
+export default ImageTile;
