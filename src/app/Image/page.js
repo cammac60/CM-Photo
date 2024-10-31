75441eb9689373b1847acfbd317abe5c3ee3a5f6
unknown <cam.mac.cm@gmail.com>
2024-10-31 13:50:29 -0600

    Add basic image component

---
+++ src/app/Image/page.js
@@ -0,0 +1,12 @@
+
+
+const Image = async props => {
+    const { id, url, caption, variantSize } = props;
+    return (
+        <div id="towers-wrapper" className="gallery-images-wrapper">
+
+        </div>
+    );
+};
+
+export default Image;
