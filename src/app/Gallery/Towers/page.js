615fc97c6bd0ac0585b7cc16f29cca0c12ac1b09
unknown <cam.mac.cm@gmail.com>
2025-01-17 20:12:32 -0700

    Remove gallery info from gallery header

--- src/app/Gallery/Towers/page.js
+++ src/app/Gallery/Towers/page.js
@@ -10,9 +10,6 @@ const Towers = async () => {
                 <h3 className='gallery-info_header'>
                     Towers
                 </h3>
-                <p className='gallery-info_text'>
-                A central theme in my photography is isolation. The Towers project aims to capture many different kinds of architectural beauty in the form of temples, skyscrapers, bell towers, and more. The concept behind the Towers project is creating portraits of these structures by isolating them from their surroundings to highlight their architectural detail only.
-                </p>
             </div>
             {data.map((item, index) => (
                 <ImageTile key={index} props={item} />
