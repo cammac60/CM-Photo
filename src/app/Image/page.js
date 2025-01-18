241f74434472d0d28db1057b88fd103bb755d1db
unknown <cam.mac.cm@gmail.com>
2025-01-17 20:41:36 -0700

    Update image component to display dyanmically

--- src/app/Image/page.js
+++ src/app/Image/page.js
@@ -1,9 +1,23 @@
 'use client';
 import Image from 'next/image';
 import './Image.scss';
+import { useState } from 'react';
 
 const ImageTile = props => {
-    const { url, caption, variantSize, order } = props.props;
+    const [currentImageNum, setCurrentImageNum] = useState(0);
+    const lastPage = props.props.length - 1;
+    const updateImage = type => {
+        setCurrentImageNum(prevNum => {
+            if (type === 'next') {
+                return prevNum === lastPage ? 0 : prevNum + 1;
+            }
+            if (type === 'previous') {
+                return prevNum === 0 ? lastPage : prevNum - 1;
+            }
+            return prevNum;
+        });
+    };
+    let { url, caption, variantSize } = props.props[currentImageNum];
     let baseHeight, baseWidth, alignment;
     switch (variantSize) {
         case 'portraitWeb':
@@ -25,8 +39,15 @@ const ImageTile = props => {
     };
     return (
         <div className={`gallery-img_${variantSize} gallery-img`}>
+            <div className='gallery-img_menu'>
+                <p className={`gallery-img_caption img-alignment_${alignment}`}>{caption}</p>
+                <div className='gallery-img_menu-controls'>
+                    <button onClick={() => updateImage('next')}>Next</button>
+                    <button onClick={() => updateImage('previous')}>Previous</button>
+                </div>
+                <a>Purchase</a>
+            </div>
             <Image src={url} className={`${variantSize} img-alignment_${alignment}`} alt="Image from API" quality={100} overrideSrc='/Img.jpg' height={baseHeight} width={baseWidth} />
-            <p className={`gallery-img_caption img-alignment_${alignment}`}>{caption}</p>
         </div>
     );
 };
