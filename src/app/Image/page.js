541ac3b0427d8c4ac72fb08b8034b120d6b9a41a
unknown <cam.mac.cm@gmail.com>
2025-01-20 20:02:00 -0700

    Update basic styling for image component

--- src/app/Image/page.js
+++ src/app/Image/page.js
@@ -18,7 +18,7 @@ const ImageTile = props => {
         });
     };
     let { url, caption, variantSize } = props.props[currentImageNum];
-    let baseHeight, baseWidth, alignment;
+    let baseHeight, baseWidth;
     switch (variantSize) {
         case 'portraitWeb':
             baseWidth = 1638;
@@ -39,16 +39,23 @@ const ImageTile = props => {
     };
     return (
         <div className={`gallery-img_${variantSize} gallery-img`}>
-                <div className='gallery-img_menu-controls'>
-                    <button className='gallery-img_menu-button' onClick={() => updateImage('previous')}>Prev</button>
-                    <p className='gallery-img_menu-divider'>/</p>
-                    <button className='gallery-img_menu-button' onClick={() => updateImage('next')}>Next</button>
+            <div className='gallery-img_main-panel'>
+                <div className='gallery-img-menu'>
+                    <div className='gallery-img_menu-text'>
+                        <p className={`gallery-img_caption`}>{caption}</p>
+                        <div className='gallery-img_store-wrapper'>
+                            <a className='gallery-img_store-link'>Purchase</a>
+                            <Image className='store-link_image' src='/images/external-link-svgrepo-com.svg' alt='Link to purchase' height={15} width={15} />
+                        </div>
+                    </div>
+                    <div className='gallery-img_menu-controls'>
+                        <button className='gallery-img_menu-button' onClick={() => updateImage('previous')}>Prev</button>
+                        <p className='gallery-img_menu-divider'>/</p>
+                        <button className='gallery-img_menu-button' onClick={() => updateImage('next')}>Next</button>
+                    </div>
                 </div>
-            <div className='gallery-img_menu'>
-                <p className={`gallery-img_caption img-alignment_${alignment}`}>{caption}</p>
-                <a>Purchase</a>
+                <Image src={url} className={`${variantSize}`} alt="Image from API" quality={100} overrideSrc='/Img.jpg' height={baseHeight} width={baseWidth} />
             </div>
-            <Image src={url} className={`${variantSize} img-alignment_${alignment}`} alt="Image from API" quality={100} overrideSrc='/Img.jpg' height={baseHeight} width={baseWidth} />
         </div>
     );
 };
